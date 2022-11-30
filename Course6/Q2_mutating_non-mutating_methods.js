c7dee7e412846748c7cdf34af673c04596d2c8b9
Shubham More <shubham.more@joshsoftware.com>
2022-11-30 18:49:42 +0530

    Added Solution 2 file

---
+++ Course6/Q2_mutating_non-mutating_methods.js
@@ -0,0 +1,33 @@
+/*
+2. What makes a method mutating or non mutating in Javascript? Find out whether each of the following methods are mutating or non-mutating. How can you verify this?:
+
+push
+pop
+filter
+find
+sort
+map
+*/
+
+/*
+Mutating methods-
+Methods which make changes in same array, without creating new ones.
+Eg:-
+.push() — Adds a new item as the last item of the array.
+.pop() — Removes the last item of the array.
+.unshift() — Adds a new item as the first item of the array.
+.shift() — Removes the first item of the array.
+.reverse() — Reverses the order of the array.
+.splice() — Removes/replaces item(s) in the array.
+.sort() — Re-orders the items in the array based on their Unicode code points.
+.copyWithin() — Copies one part of the array and puts it on another part of the same array.
+.fill() — Changes some or all items in the array into the value being passed in.
+
+Non-Mutating-
+Methods which returns new array as result.
+Eg:-
+.filter()-creates new array on the basis of given condition 
+.find()-returns the first index of the given element that satisfies the condition
+.map()-returns the new array by applying the various operation given to it.
+
+*/
