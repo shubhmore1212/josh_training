01466024b63f81485ccd86a48d005b08a36e61c0
Shubham More <shubham.more@joshsoftware.com>
2022-11-30 11:39:25 +0530

    Accessed the char of string as array

--- Course5/Q2_string_operation.js
+++ Course5/Q2_string_operation.js
@@ -11,11 +11,18 @@ console.log("String Length", str.length);
   Accessing char that is out of range i.e. char after 5 lets say 6
 */
 
+console.log("Accessing char by string array", str[5]);
+//array act as a object if we pass index out of range, it returns undefined
+
 console.log("Accessing char out of range", str.charAt(6));
 //returns empty string('') if try to access the character that is not in range
 
 /*
 Passing negative index to charAt string method
 */
-console.log("Passing negative index", str.charAt(-1) + "s");
+
+console.log("Accessing char by string array", str[-1]);
+//array act as a object if we pass index as negative, it returns undefined
+
+console.log("Passing negative index", str.charAt(-1));
 //returns empty string('') if try to access the character by giving negqative index
