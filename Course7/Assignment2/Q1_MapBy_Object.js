fb71c2dba04d8dfc29aaeba9a1a6598fa8eabc07
Shubham More <shubham.more@joshsoftware.com>
2022-12-02 16:26:14 +0530

    Question 1 Solution file added

---
+++ Course7/Assignment2/Q1_MapBy_Object.js
@@ -0,0 +1,45 @@
+/*
+Write a function mapBy to convert an array of objects into an object mapped by the specified key:
+Example: 
+ Let users = [{
+"id": 1,
+"first_name": "Nicki",
+"email": "ncrozier0@squarespace.com",
+"date_of_birth": "2009/05/09"
+}, {
+"id": 2,
+"first_name": "Raychel",
+"email": "rmcgrady1@cpanel.net",
+"date_of_birth": "1996/11/05"
+}];
+mapBy(users, “first_name”) 
+ 
+//This should return
+//{
+// “Nicki”:{id:1, first_name:”Nicki”, ...},
+// “Raychel”:{id:2, first_name:”Raychell”, ...},
+//}
+*/
+
+let users = [
+  {
+    id: 1,
+    first_name: "Nicki",
+    email: "ncrozier0@squarespace.com",
+    date_of_birth: "2009/05/09",
+  },
+  {
+    id: 2,
+    first_name: "Raychel",
+    email: "rmcgrady1@cpanel.net",
+    date_of_birth: "1996/11/05",
+  },
+];
+
+const mapBy = (users, value) => {
+  let result = {};
+  users.map((user) => (result[user[value]] = user));
+  return result;
+};
+
+console.log(mapBy(users, "first_name"));
